84c9d4da7ab2e910dc311f6de3fac2a241fb2296
Lázaro <lazarodm@gmail.com>
2014-04-11 13:28:58 -0300

    - Adicionado o arquivo dependencias.pip com as dependencias necessárias para o projeto
    - Adicionado o método __unicode__ para equipes.models.Equipe

--- site/equipes/models.py
+++ site/equipes/models.py
@@ -7,3 +7,7 @@ class Equipe(models.Model):
     Representa uma equipe que participará de campeonatos
     """
     nome = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        
+        return self.nome
