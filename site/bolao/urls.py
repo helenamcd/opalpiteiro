6dda45b16f90a5f9a34559eedb85cf2c628c770c
Paulo Calanca <paulinho.calanca@gmail.com>
2014-04-11 22:28:24 -0300

    modificado css e html

--- site/bolao/urls.py
+++ site/bolao/urls.py
@@ -14,7 +14,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cadastro/$', CadastroUsuario.as_view()),
-    url(r'^$', TemplateView.as_view(template_name="embreve.html")),
+    #url(r'^$', TemplateView.as_view(template_name="embreve.html")),
+	url(r'^$', TemplateView.as_view(template_name="index.html")),
+
 )
 
 # Serve a mídia estática enquanto está em DEBUG
