12b57bf2cd1fe4605f458a93d580707a6e39f56f
PauloCalanca <paulinho.calanca@gmail.com>
2014-04-15 21:11:41 -0300

    Foi alterado o css e o html da index e feito os ultimos testes e adequação do menu lateral.

--- site/static/js/script.js
+++ site/static/js/script.js
@@ -5,23 +5,33 @@ $(function(){
 		if (verificaMenu) {
 			$("#menuLeftBar").removeClass('transitionLeftToRight');
 			$("#menuLeftBar").addClass('transitionRightToLeft');
-			
 			$("#contextCenter").css("width","100%");
 			$("#contextCenter").css("margin-left","0%");
-			$("#containerPalpiteRodada>button").css("width","8.3%");
-			$("#menuLeftBar").css("margin-left","-5%");
+			
+			$("#menuLeftBar").css("margin-left","-8%");
 			verificaMenu=false;
 		} else{
 			$("#menuLeftBar").addClass('transitionLeftToRight');
 			$("#menuLeftBar").css('margin-top',$(window).scrollTop())
-			$("#contextCenter").css("width","95%");
-			$("#contextCenter").css("margin-left","5%");
-			$("#containerPalpiteRodada>button").css("width","7%");
+			$("#contextCenter").css("width","92%");
+			$("#contextCenter").css("margin-left","8%");
+			
 			$("#menuLeftBar").css("margin-left","0%");
 			verificaMenu=true;
 		};
 		
 	});
+
+	$(".barraMenuLeft").hover(
+	  function() {
+	    $(this).css("width","100%");
+	    
+	  },
+	  function() {
+	    $(this).css("width","40%");
+	    
+	  }
+	);
 
 	$(window).scroll(function() {
 	  if (verificaMenu) {
